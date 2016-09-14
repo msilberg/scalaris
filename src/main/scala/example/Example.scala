10889eafb152efde0653c95669a8ba426726c69a
mike <michaels@avantisteam.com>
2016-09-14 17:21:27 +0300

    Scalaris rv0.2

---
+++ src/main/scala/example/Example.scala
@@ -0,0 +1,8 @@
+package example
+
+/**
+  * Created by mike on 14/09/16.
+  */
+object Example extends App {
+  println("Hello World Mike")
+}
---
+++ src/main/scala/pioneer.sc
@@ -0,0 +1,3 @@
+"hello world"
+2*3
+scala.math.pow(2,3)
